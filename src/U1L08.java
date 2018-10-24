6788d66047886f52e12258eb2abb363243d1f5eb
Isabelle <hammeri@theellisschool.org>
2018-10-24 15:02:13 -0400

    this is what I have right now...not printing the correct numbers

---
+++ src/U1L08.java
@@ -0,0 +1,31 @@
+import java.util.Collections;
+
+public class U1L08 {
+
+	public static void main(String[] args) {
+	
+		//reverse the order without creating a second array
+		//you can assign the array values when you declare it, but program should work for... 
+		//...any array (changed/added values)
+		//hint: write out algorithm in pseudocode
+		
+		int [] array = {1, 3, 5};
+		
+		int i = 0;
+		
+		
+		for(i = 0; i < array.length; i++){
+			int temp = array[i];
+			array[i] = array[array.length -i -1];
+			array[array.length -i -1] = temp;
+			System.out.println(temp);//whatever is here needs to save, remove, and replace the number
+		}
+		 
+		
+		
+		
+
+	}
+
+
+}
